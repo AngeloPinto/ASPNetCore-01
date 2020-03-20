d413a4d7b7f647bd37ec12daf4e305b1732eb625
Angelo <angelorpt@gmail.com>
2020-03-20 16:00:49 -0400

    Personalizando as rotas

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,13 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASPCORE.Controllers
 {
+    [Route("painel/admin")]
     public class AdminController : Controller
     {
+        [HttpGet("")]
+        [HttpGet("principal")]
         public IActionResult Index()
         {
             return Content("Exibindo painel admin");
         }
 
+        [HttpGet("teste")]
         public IActionResult Teste()
         {
             return Content("Resultado do teste");
